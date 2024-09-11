72ec04bc1c522f174d03042982aa47a7803702ea
Nick Dalalelis <ndalalelis@gmail.com>
2024-09-11 07:55:31 -0400

    unit tests

--- tests/test_random.py
+++ tests/test_random.py
@@ -1,12 +1,8 @@
-import unittest
+import pytest
 
 from nickspylib.random import generate_random_number
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        num = generate_random_number(3, 10)
-        self.assertTrue(num >= 3)
-        self.assertTrue(num < 10)
-
-if __name__ == '__main__':
-    unittest.main()
+def test_generate_random_number():
+    num = generate_random_number(3, 10)
+    assert num >= 3
+    assert num < 10
